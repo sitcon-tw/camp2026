8e7f23964d72d09cbc213d2535dd66de8d6c7156
Yorukot <hi@yorukot.me>
2026-04-26 21:02:20 +0800

    refactor: it's time to refactor this website

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,6 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
-import favicons from "astro-favicons";
-
-import partytown from "@astrojs/partytown";
-
 const base = "/2026";
 
 // https://astro.build/config
@@ -17,18 +13,4 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [
-    favicons({
-      name: "SITCON Camp 2026",
-      short_name: "SITCON Camp",
-      output: {
-        images: true,
-        files: true,
-        html: true,
-        assetsPrefix: base,
-      },
-    }),
-    partytown(),
-  ],
 });
