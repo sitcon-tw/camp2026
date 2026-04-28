3dc7204d776a61ff22ffc1e03aba88ec7187b097
Yorukot <hi@yorukot.me>
2026-04-28 12:14:47 +0800

    chore: add prepare & preinstall % add allow only use pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,44 +7,44 @@ const base = "/2026";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sitcon.camp",
-  base,
+	site: "https://sitcon.camp",
+	base,
 
-  fonts: [
-    {
-      provider: fontProviders.local(),
-      name: "LINE Seed TW",
-      cssVariable: "--font-line-seed-tw",
-      weights: [250, 400, 700, 800],
-      styles: ["normal"],
-      options: {
-        variants: [
-          {
-            weight: 250,
-            style: "normal",
-            src: ["./src/assets/fonts/LINESeedTW_OTF_Th.woff2"],
-          },
-          {
-            weight: 400,
-            style: "normal",
-            src: ["./src/assets/fonts/LINESeedTW_OTF_Rg.woff2"],
-          },
-          {
-            weight: 700,
-            style: "normal",
-            src: ["./src/assets/fonts/LINESeedTW_OTF_Bd.woff2"],
-          },
-          {
-            weight: 800,
-            style: "normal",
-            src: ["./src/assets/fonts/LINESeedTW_OTF_Eb.woff2"],
-          },
-        ],
-      },
-    },
-  ],
+	fonts: [
+		{
+			provider: fontProviders.local(),
+			name: "LINE Seed TW",
+			cssVariable: "--font-line-seed-tw",
+			weights: [250, 400, 700, 800],
+			styles: ["normal"],
+			options: {
+				variants: [
+					{
+						weight: 250,
+						style: "normal",
+						src: ["./src/assets/fonts/LINESeedTW_OTF_Th.woff2"]
+					},
+					{
+						weight: 400,
+						style: "normal",
+						src: ["./src/assets/fonts/LINESeedTW_OTF_Rg.woff2"]
+					},
+					{
+						weight: 700,
+						style: "normal",
+						src: ["./src/assets/fonts/LINESeedTW_OTF_Bd.woff2"]
+					},
+					{
+						weight: 800,
+						style: "normal",
+						src: ["./src/assets/fonts/LINESeedTW_OTF_Eb.woff2"]
+					}
+				]
+			}
+		}
+	],
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	vite: {
+		plugins: [tailwindcss()]
+	}
 });
